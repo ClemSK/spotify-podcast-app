77a5897a37c8c146fe1d15264797b34fed23dd14
Clement Seconde-Kynnersely <c.sec.kyn@gmail.com>
2021-08-28 18:32:28 +0100

    start

--- src/common/NavBar.js
+++ src/common/NavBar.js
@@ -1,1 +1,18 @@
-import React
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <div>
+        <div>
+          <Link to="/" className="navbar-item">
+            Home
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
