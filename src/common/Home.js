b8e589c9854cc7b88842ed02991a73b32697f297
James Gore <jamesgore@Jamess-MacBook-Pro.local>
2021-08-30 21:41:04 +0100

    latest version

--- src/common/Home.js
+++ src/common/Home.js
@@ -1,44 +1,10 @@
-import React, { useEffect } from 'react'
-// import { useHistory } from 'react-router-dom'
+import React  from 'react'
 import Login from '../auth/Login'
-import { getAccessToken } from '../lib/SpotifyAuth'
 
 const Home = () => {
-  // const history = useHistory()
-  const [state, setState] = React.useState(null)
-
-  // This is monitoring the URL so when it changes we land back on the page and can take the access token
-  useEffect(() => {
-    const urlToken = getAccessToken()
-    window.location.hash = '' // removes the access token from the URL
-    console.log(`We now have an access token pulled from the url:`, urlToken)
-    const token = urlToken.access_token
-    console.log(`The barer token is:`, token)
-    if (token) {
-      setState(token)
-    }
-  }, [])
-
-  return <section>{state ? <h1>you are logged in!</h1> : <Login />}</section>
-  // React.useEffect(() => {
-  //   SpotifyAuth()
-  // }, [])
+return (
+  <Login />
+)
 }
-
-// const settingTokenInBrowser = async (e) => {
-//   try {
-//     const res = await setState(token)
-//     if (res.status === 200) {
-//       // callback()
-//       history.push('/podcast')
-//     }
-//   } catch (err) {
-//     console.error('Error setting bearer token', err)
-//   }
-// }
-// return settingTokenInBrowser
-
-// maybe have a setInterval function to
-// redirect to podcast page after 3s?
 
 export default Home
---
+++ src/components/GetPodcast.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const podcastEndpoint = "https://api.spotify.com/v1/me/shows";
+
+const GetPodcast = () => {
+  const [token, setToken] = useState("");
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    if (localStorage.getItem("accessToken")) {
+      setToken(localStorage.getItem("accessToken"));
+    }
+  }, []);
+
+  const handleGetPodcasts = () => {
+    axios
+      .get(podcastEndpoint, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((err) => {
+        console.error("there was an error fetching podcasts", err);
+      });   
+  };
+  
+  return (
+    <>
+      <button onClick={handleGetPodcasts}>Get Podcasts</button>
+      {data?.items ? data.items.map((item) => <p>{item.name}</p>) : null}
+    </>
+  );
+};
+
+export default GetPodcast;
