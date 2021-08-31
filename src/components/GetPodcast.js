beba87f76715b18705b67a19130af1afdde97d91
Clement Seconde-Kynnersely <c.sec.kyn@gmail.com>
2021-08-31 20:26:45 +0100

    getting-the-data

--- src/components/GetPodcast.js
+++ src/components/GetPodcast.js
@@ -1,39 +1,52 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-const podcastEndpoint = "	https://api.spotify.com/v1/me/episodes";
+// const podcastEndpoint = 'https://api.spotify.com/v1/me/shows'
+const podcastEndpoint =
+  'https://api.spotify.com/v1/shows/6RbJUsaOaboqSBqQUfdQtR/episodes'
 
 const GetPodcast = () => {
-  const [token, setToken] = useState("");
-  const [data, setData] = useState({});
+  const [token, setToken] = useState('')
+  const [data, setData] = useState({})
 
   useEffect(() => {
-    if (localStorage.getItem("accessToken")) {
-      setToken(localStorage.getItem("accessToken"));
+    if (localStorage.getItem('accessToken')) {
+      setToken(localStorage.getItem('accessToken'))
     }
-  }, []);
+  }, [])
 
   const handleGetPodcasts = () => {
     axios
       .get(podcastEndpoint, {
         headers: {
-          Authorization: "Bearer " + token,
+          Authorization: 'Bearer ' + token,
         },
       })
       .then((response) => {
-        setData(response.data);
+        setData(response.data)
       })
       .catch((err) => {
-        console.error("there was an error fetching podcasts", err);
-      });   
-  };
-  
+        console.error('there was an error fetching podcasts', err)
+      })
+  }
+
   return (
     <>
       <button onClick={handleGetPodcasts}>Get Podcasts</button>
-      {data?.items ? data.items.map((item) => <p>{item.name}</p>) : null}
+      {data?.items
+        ? data.items.map((item) => (
+            <p>
+              {item.name}
+              {/* {item.show.name}
+              {', '}
+              {item.show.total_episodes}
+              {', '}
+              {item.show.publisher} */}
+            </p>
+          ))
+        : null}
     </>
-  );
-};
+  )
+}
 
-export default GetPodcast;
+export default GetPodcast
