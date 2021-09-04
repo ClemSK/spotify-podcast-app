54249ebbe3814404b84b6e39d038160fafca9e8d
James Gore <83005220+thejamesgore@users.noreply.github.com>
2021-09-04 15:42:39 +0100

    Update SearchCard.js

--- src/components/SearchCard.js
+++ src/components/SearchCard.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 function SearchCard(results) {
-  const name = results.name
-  const image = results.images[0].url
-  const publisher = results.publisher
-  const description = results.description
+const name = results.name
+const image = results.images[0].url
+const publisher = results.publisher
+const description = results.description
 
   return (
     <div className="column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
@@ -16,7 +16,6 @@ function SearchCard(results) {
         </div>
 
         <div className="card-image">
-
             <figure className="image is-1by1">
               <img
                 src={image}
@@ -38,31 +37,9 @@ function SearchCard(results) {
           </div>
         
 
-          <figure className="image is-1by1">
-            <img
-              src={image}
-              alt={name}
-              loading="lazy"
-              width="200"
-              hieght="200"
-            />
-          </figure>
-        </div>
-
-
-        <div className="card-content">
-          <h5>{publisher}</h5>
-          <h5>
-            {description.length >= 50
-              ? description.slice(0, 50) + '...'
-              : description}
-          </h5>
-        </div>
       </div>
-    
+    </div>
   )
 }
 
 export default SearchCard
-
-
